eadac8b80297807493c6e2b0c8b92c21bd211dfc
Ryan <ryanpaynt@uncc.edu>
2021-10-26 09:48:39 -0400

    flask app #3 - added execution path for POST request for new note

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -4,16 +4,20 @@
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)     # create an app
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
+
+
 @app.route('/')
 def index():
-    a_user = {'name': 'Mogli', 'email': 'mogli@uncc.edu'}
-    return render_template("index.html", user = a_user)
+    a_user = {'name': 'Ryan', 'email': 'ryan@uncc.edu'}
+    return render_template("index.html", user=a_user)
+
 
 @app.route('/notes')
 def get_notes():
@@ -21,8 +25,9 @@ def get_notes():
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
              }
+    a_user = {'name': 'Ryan', 'email': 'ryan@uncc.edu'}
+    return render_template("notes.html", notes=notes, user=a_user)
 
-    return render_template("notes.html", notes = notes)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
@@ -30,15 +35,22 @@ def get_note(note_id):
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
              }
+    a_user = {'name': 'Ryan', 'email': 'ryan@uncc.edu'}
+    return render_template("note.html", note=notes[int(note_id)], user=a_user)
 
-    return render_template("note.html", note = notes[int(note_id)])
 
-@app.route('/notes/new')
+@app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
-  a_user = {'name': 'Mogli', 'email': 'mogli@uncc.edu'}
-  return render_template('new.html', user = a_user)
+    a_user = {'name': 'Mogli', 'email': 'mogli@uncc.edu'}
+    print('request method is', request.method)
+    if request.method == 'POST':
+        return '<h1> POST method used for this request </h1>'
+    else:
+        return render_template('new.html', user=a_user)
 
-app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
+
+app.run(host=os.getenv('IP', '127.0.0.1'), port=int(
+    os.getenv('PORT', 5000)), debug=True)
 
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
